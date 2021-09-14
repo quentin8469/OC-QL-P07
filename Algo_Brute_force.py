ca06c0c7bb4cc0507d096e17811eb0d3fa530759
Quentin Laparra <quentin.laparra@gmail.com>
2021-09-14 15:48:38 +0200

    WIP pseudo code

--- Algo_Brute_force.py
+++ Algo_Brute_force.py
@@ -2,4 +2,15 @@
 # import itertools
 
 
-# 
+def main():
+    """"""
+    # ouvrir mon fichier dataset.csv
+    # lire les datas de mon fichier
+    # calculer les gains de chaque actions sur 2 ans ((coût*benef)/100)
+    # enregistrer les gains pour chaque actions
+    # tester chaques combinaisons possible d'action pour un investisement max de 500€ et une rentabilité maximale
+    # afficher la reponse
+    pass
+
+if __name__ == "__main__":
+    main()
