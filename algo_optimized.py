e6f8629d94c3a31da5a6d071a5b03ad75ee8acc7
Quentin Laparra <quentin.laparra@gmail.com>
2021-09-19 23:24:34 +0200

    WIP on def glouton

--- algo_optimized.py
+++ algo_optimized.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import combinations
 from os import name
 
 
@@ -14,30 +15,64 @@ class Action:
     def __str__(self):
         return f'Name: {self.name}, Price: {self.price}, Profit: {self.profit}, Gain: {self.gain}'
 
+
 def data_set_csv(datafile):
     """Open and read the csv file"""
     wallet =[]
     with open(f'data_set/{datafile}.csv', newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-	        wallet.append(Action(row[0], float(row[1]), float(row[2])))
+            if float(row[1]) <= 0.0 or float(row[2]) <= 0.0:
+	            pass
+            else:
+                wallet.append(Action(row[0], float(row[1]), float(row[2])))
     return wallet
 
 
 def glouton(wallet, max_invest):
-    """ try to find a good invest """
+    """try to find a good invest"""
     #trier les objets action par gain possible
+    best_combination = []
+    total_gain = 0
+    total_invest = 0
     wallet_tri = sorted(wallet, key=lambda x:x.gain, reverse=True)
-    for tes in wallet_tri:
-        print('test tri glouton:', tes.name , tes.price, tes.gain)
+    for wal in wallet_tri:
+        print(wal)
+    for wallet in wallet_tri:
+        #print(wallet.gain)
+        invest = total_invest + wallet.price
+        if invest <= max_invest and wallet.gain >0:
+            total_invest = invest
+            total_gain += wallet.gain
+            best_combination.append(wallet.price)      
+        #print(wallet.name, total_invest, total_gain)
+    return f'La meilleure combinaison d\'action est: {best_combination}\n'\
+           f'Pour un gain estimé de: {total_gain} €\n'\
+           f'Pour un investissement de:{total_invest} €'
 
 
 def main():
     """"""
-    max_invest = 500
-    csv_file = input('dataset0 ou dataset1_Python+P7 ou dataset2_Python+P7 :' )
+    
+    print("----------------------------------------------")
+    print("             Programme Optimized              ")
+    print("----------------------------------------------")
+    print("1:dataset0   2:dataset1_Python+P7   3:dataset2_Python+P7")
+        
+    algo = input("Entrez votre choix et appuyez sur entrée: ")
+    if algo == '1':
+        csv_file = 'dataset0'
+    elif algo == '2':
+        csv_file = 'dataset1_Python+P7'
+    elif algo == '3':
+        csv_file = 'dataset2_Python+P7'
+    
+    max_invest = 500        
     wallet = data_set_csv(csv_file)
+    #for wal in wallet:
+        #print(wal)
     opti_invest = glouton(wallet, max_invest)
+    print(opti_invest)
 
 if __name__ == "__main__":
     """"""
