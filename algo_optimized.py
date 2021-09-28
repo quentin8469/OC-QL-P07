daff00a0aea3a39d60bb118879f7748d01f7a59b
Quentin Laparra <quentin.laparra@gmail.com>
2021-09-29 00:11:13 +0200

    WIP def best_option, find the best portfolio with the matrix

--- algo_optimized.py
+++ algo_optimized.py
@@ -1,6 +1,5 @@
 import csv
-from itertools import combinations
-from os import name
+
 
 
 
@@ -11,7 +10,7 @@ class Action:
         self.price = int(price)
         self.profit = int(profit)
         self.gain = self.price*self.profit/100
-        
+
     def __str__(self):
         return f'Name: {self.name}, Price: {self.price}, Profit: {self.profit}, Gain: {self.gain}'
 
@@ -23,10 +22,10 @@ def data_set_csv(datafile):
         reader = csv.reader(csvfile)
         for row in reader:
             if float(row[1]) <= 0.0 or float(row[2]) <= 0.0:
-	            del(row)    
+	            del(row)
             else:
                 wallet.append(Action(row[0], float(row[1]), float(row[2])))
-                
+
     return wallet
 
 
@@ -38,13 +37,13 @@ def get_matrix(wallet, max_invest):
         for hauteur in range(0, max_invest +1):
             tableau_largeur.append(0)
         matrix.append(tableau_largeur)
-    
+
     return matrix
 
-    
+
 def algo_sac_a_dos(wallet, max_invest):
     """"""
-    matrice = get_matrix(wallet, max_invest) 
+    matrice = get_matrix(wallet, max_invest)
     for action in range(1, len(wallet)+1):
         for invest in range(1, max_invest+1):
             if wallet[action-1].price <= invest:
@@ -53,30 +52,34 @@ def algo_sac_a_dos(wallet, max_invest):
             else:
                 matrice[action][invest] = matrice[action-1][invest]
     return matrice
-    
+
+
 def best_option(wallet, max_invest, matrice):
     """"""
-    k = max_invest
+
     n = len(wallet)
     optimized_portfolio = []
-    
-    while k >= 0 and n >= 0:
-        e = wallet[n-1]
-        if matrice[n][k] == matrice[n-1][k- e.price]+ e.profit:
-            optimized_portfolio.append(e)
-            k -= e.price
-        n -=1
+
+    while max_invest >= 0 and n >= 0:
+        action = wallet[n-1]
+
+        if matrice[n][max_invest] == matrice[n-1][max_invest-action.price] + action.profit:
+            print(matrice[n][max_invest])
+        #     print(matrice[n][max_invest])
+        #     optimized_portfolio.append(e)
+        #     k -= e.price
+        # n -=1
     return optimized_portfolio
-    
+
 
 def main():
     """"""
-    
+
     print("----------------------------------------------")
     print("             Programme Optimized              ")
     print("----------------------------------------------")
     print("1:dataset0   2:dataset1_Python+P7   3:dataset2_Python+P7")
-        
+
     algo = input("Entrez votre choix et appuyez sur entrée: ")
     if algo == '1':
         csv_file = 'dataset0'
@@ -84,13 +87,13 @@ def main():
         csv_file = 'dataset1_Python+P7'
     elif algo == '3':
         csv_file = 'dataset2_Python+P7'
-    
+
     max_invest = 50 #conversion euros en centimes *100
     wallet = data_set_csv(csv_file)
     print("------------sac à dos-------------------")
     print("----------------------------------------------")
     sac_a_dos = algo_sac_a_dos(wallet, max_invest)
-    print('mon resultat', sac_a_dos)
+    #print('mon resultat', sac_a_dos)
     best_portfolio = best_option(wallet, max_invest, sac_a_dos)
     print('mon resultat final', best_portfolio)
     print("--------------END sac à dos---------------------")
